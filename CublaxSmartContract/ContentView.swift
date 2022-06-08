56284bf289c7cd3e70a333f91f6d213e0d5869cd
Alexandre Quiblier <alexandre.quiblier@meona.de>
2022-06-08 12:09:31 +0900

    Initial Commit

---
+++ CublaxSmartContract/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CublaxSmartContract
+//
+//  Created by Meona on 08.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CublaxSmartContract/CublaxSmartContractApp.swift
@@ -0,0 +1,17 @@
+//
+//  CublaxSmartContractApp.swift
+//  CublaxSmartContract
+//
+//  Created by Meona on 08.06.22.
+//
+
+import SwiftUI
+
+@main
+struct CublaxSmartContractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
